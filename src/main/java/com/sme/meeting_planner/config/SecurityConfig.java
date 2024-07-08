1d48db10f3cf77a855b192abbd3e9297f3c8dc79
ismahane arris <ismahane.arris@pinitservices.com>
2024-07-08 08:30:56 +0100

    MP-00/remove-unnecessary-imports

--- src/main/java/com/sme/meeting_planner/config/SecurityConfig.java
+++ src/main/java/com/sme/meeting_planner/config/SecurityConfig.java
@@ -3,11 +3,8 @@ package com.sme.meeting_planner.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
     private final JwtFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
@@ -25,8 +21,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests(req -> req.requestMatchers("/",
                         "/auth/**",
                         "/index.html",
